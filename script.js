70a9bfd968fb74155caa3dc86f6d323d3c098cfd
Bruno Silva <soy.brunosilva@gmail.com>
2023-08-09 09:18:09 -0300

    Adding the first features of this mini project

---
+++ script.js
@@ -0,0 +1,13 @@
+let wins = 0;
+let userChoice = 0;
+let botChoice = 0;
+
+// Game Loop
+let rockBtn = document.getElementById('rock-btn');
+let scissorsBtn = document.getElementById('scissor-btn');
+let paperBtn = document.getElementById('paper-btn');
+
+rockBtn.addEventListener('click', fun)
+scissorsBtn.addEventListener('click', fun)
+paperBtn.addEventListener('click', fun)
+
