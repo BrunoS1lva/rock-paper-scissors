b39ddfc5df582efb3db6921179e33d8e2619c9db
Bruno Silva <soy.brunosilva@gmail.com>
2023-08-09 09:54:15 -0300

    Trying to make the code work!

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
-let wins = 0;
-let userChoice = 0;
-let botChoice = 0;
+// let wins = 0;
+let botChoice = undefined;
 
-// Game Loop
-let rockBtn = document.getElementById('rock-btn');
-let scissorsBtn = document.getElementById('scissor-btn');
-let paperBtn = document.getElementById('paper-btn');
+let buttons = ['rockBtn','scissorsBtn','paperBtn'];
+buttons.forEach(button => {
+  button = document.getElementById(button);
+});
+// let rockBtn = document.getElementById("rock-btn");
+// let scissorsBtn = document.getElementById("scissors-btn");
+// let paperBtn = document.getElementById("paper-btn");
 
-rockBtn.addEventListener('click', fun)
-scissorsBtn.addEventListener('click', fun)
-paperBtn.addEventListener('click', fun)
+rockBtn.addEventListener("click", userChoice);
+scissorsBtn.addEventListener("click", userChoice);
+paperBtn.addEventListener("click", userChoice);
 
+
+botChoice = fun();
+console.log(`Bot Choice is ${botChoice}`);
+
+function fun() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return (botChoice = "rock");
+      break;
+    case 1:
+      return (botChoice = "scissors");
+      break;
+    case 2:
+      return (botChoice = "paper");
+      break;
+    default:
+      alert("ERROR! 💀");
+  }
+}
+function userChoice() {
+  alert('eureka!')
+  // switch (number) {
+  //   case 0:
+  //     return (userAttack = "rock");
+  //     break;
+  //   case 1:
+  //     return (userAttack = "scissors");
+  //     break;
+  //   case 2:
+  //     return (userAttack = "paper");
+  //     break;
+  //   default:
+  //     alert("ERROR! 💀");
+  // }
+}
+function gameOn(userAttack){
+  if(userAttack == 'rock' && botChoice == 'scissors'){
+    document.write('You won!');
+  }
+}
