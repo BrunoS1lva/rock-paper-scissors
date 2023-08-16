53830c1cedbc9684fa696eda7ddba84d5d3cc111
Bruno Silva <soy.brunosilva@gmail.com>
2023-08-16 18:51:37 -0300

    Improving the script and the logic of the game

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 // let wins = 0;
-let botChoice = undefined;
 
-let buttons = ['rockBtn','scissorsBtn','paperBtn'];
-buttons.forEach(button => {
-  button = document.getElementById(button);
-});
-// let rockBtn = document.getElementById("rock-btn");
-// let scissorsBtn = document.getElementById("scissors-btn");
-// let paperBtn = document.getElementById("paper-btn");
+//Getting the elements from the html
+let rockBtn = document.getElementById("rock-btn");
+let scissorsBtn = document.getElementById("scissors-btn");
+let paperBtn = document.getElementById("paper-btn");
 
-rockBtn.addEventListener("click", userChoice);
-scissorsBtn.addEventListener("click", userChoice);
-paperBtn.addEventListener("click", userChoice);
+//Adding the eventListener to every button
+rockBtn.addEventListener("click", userChoiceDecider(rock));
+scissorsBtn.addEventListener("click", userChoiceDecider(scissors));
+paperBtn.addEventListener("click", userChoiceDecider(paper));
 
 
-botChoice = fun();
-console.log(`Bot Choice is ${botChoice}`);
+let botChoice = botChoiceMaker();
 
-function fun() {
+function botChoiceMaker() {
   let randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
     case 0:
@@ -33,24 +29,33 @@ function fun() {
       alert("ERROR! 💀");
   }
 }
-function userChoice() {
-  alert('eureka!')
-  // switch (number) {
-  //   case 0:
-  //     return (userAttack = "rock");
-  //     break;
-  //   case 1:
-  //     return (userAttack = "scissors");
-  //     break;
-  //   case 2:
-  //     return (userAttack = "paper");
-  //     break;
-  //   default:
-  //     alert("ERROR! 💀");
-  // }
+
+let userAttack = userChoiceDecider();
+
+function userChoiceDecider(attack) {
+  return attack; 
 }
 function gameOn(userAttack){
   if(userAttack == 'rock' && botChoice == 'scissors'){
-    document.write('You won!');
+    resultMaker(userAttack, botChoice, won)
+  } else if (userAttack === 'scissors' && botChoice === 'paper') {
+    resultMaker(userAttack, botChoice, won);
+  } else if (userAttack === 'paper' && botChoice === 'rock') {
+    resultMaker(userAttack, botChoice, won);
+  } else if (userAttack === botChoice) {
+    resultMaker(userAttack, botChoice, tie);
+  } else {
+    resultMaker(botChoice, userAttack, lost);
   }
 }
+function resultMaker(userAttack, botChoice, result){
+  let resultElement = document.createElement('p');
+  let resultText = document.createTextNode(`You ${result} because you chose ${userAttack} and the bot chose ${botChoice}.`)
+  let resultContainer = document.getElementById('result-container');
+  resultElement.appendChild(resultText);
+  resultContainer.appendChild(resultElement);
+}
+
+// function resultMaker(winner, loser) {
+//   console.log(winner + ' beats ' + loser + '. ' + winner + ' wins!');
+// }
